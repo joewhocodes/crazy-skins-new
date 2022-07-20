a01410e24875bf0d9c581611231596c96102937c
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-07-20 12:18:22 +0100

    add animated stars to background

--- main.js
+++ main.js
@@ -21,6 +21,31 @@
 
 
 
+// Stars
+document.addEventListener("DOMContentLoaded", function() {
+   
+  var amount = 30;
+  var body = document.querySelector('body');
+  var i = 0;
+  
+  while (i < amount) {
+  var node = document.createElement("i");
+  var posX = Math.floor(Math.random() * window.innerWidth);
+  var posY = Math.floor(Math.random() * window.innerHeight);
+  var rotation = Math.random() * 180;
+  var delay = Math.random() * 20;
+  var scale = Math.random() * 0.2;
+  node.style.left = posX+'px';
+  node.style.top = posY+'px';
+  node.style.transform = 'rotate('+rotation+'deg) scale('+scale+')';
+  node.style.animationDelay = delay+'s';
+  body.appendChild(node);
+  i++;
+  }
+  
+});
+
+// Carousel
 let items = document.querySelectorAll('#featureContainer .carousel .carousel-item');
 items.forEach((el) => {
   const minPerSlide = 6
@@ -39,3 +64,8 @@ $(document).ready(function(){
   $('#featureCarousel').carousel({interval: 2000});
   $('#featureCarousel').carousel('pause');
 });
+
+
+//  stars
+
+
